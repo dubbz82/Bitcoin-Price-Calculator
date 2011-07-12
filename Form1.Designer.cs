424f45a311e6a32634e8bd45e81fbfc2a694093b
unknown <Jeremiah@.(none)>
2011-07-12 12:01:45 -0500

    MtGox now accessable! All 3 major exchanges now supported
    Code also cleaned up a little bit, though still ugly in places

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,10 +29,10 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
-            this.lblTradeHillSell = new System.Windows.Forms.Label();
+            this.lblMtGoxSell = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.lblThBuy = new System.Windows.Forms.Label();
+            this.lblMtGoxBuy = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.lblbmsell = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
@@ -43,47 +43,52 @@
             this.textBox3 = new System.Windows.Forms.TextBox();
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.lblthsell = new System.Windows.Forms.Label();
+            this.lblthbuy = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
             this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
-            // lblTradeHillSell
+            // lblMtGoxSell
             // 
-            this.lblTradeHillSell.AutoSize = true;
-            this.lblTradeHillSell.Location = new System.Drawing.Point(148, 35);
-            this.lblTradeHillSell.Name = "lblTradeHillSell";
-            this.lblTradeHillSell.Size = new System.Drawing.Size(0, 13);
-            this.lblTradeHillSell.TabIndex = 1;
+            this.lblMtGoxSell.AutoSize = true;
+            this.lblMtGoxSell.Location = new System.Drawing.Point(148, 35);
+            this.lblMtGoxSell.Name = "lblMtGoxSell";
+            this.lblMtGoxSell.Size = new System.Drawing.Size(0, 13);
+            this.lblMtGoxSell.TabIndex = 1;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(12, 35);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(114, 13);
+            this.label1.Size = new System.Drawing.Size(100, 13);
             this.label1.TabIndex = 2;
-            this.label1.Text = "Trade Hill Sell Price : $";
+            this.label1.Text = "MtGox Sell Price : $";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Location = new System.Drawing.Point(11, 66);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(115, 13);
+            this.label2.Size = new System.Drawing.Size(99, 13);
             this.label2.TabIndex = 3;
-            this.label2.Text = "Trade Hill Buy Price : $";
+            this.label2.Text = "Mtgox Buy Price : $";
             // 
-            // lblThBuy
+            // lblMtGoxBuy
             // 
-            this.lblThBuy.AutoSize = true;
-            this.lblThBuy.Location = new System.Drawing.Point(148, 66);
-            this.lblThBuy.Name = "lblThBuy";
-            this.lblThBuy.Size = new System.Drawing.Size(0, 13);
-            this.lblThBuy.TabIndex = 4;
+            this.lblMtGoxBuy.AutoSize = true;
+            this.lblMtGoxBuy.Location = new System.Drawing.Point(148, 66);
+            this.lblMtGoxBuy.Name = "lblMtGoxBuy";
+            this.lblMtGoxBuy.Size = new System.Drawing.Size(0, 13);
+            this.lblMtGoxBuy.TabIndex = 4;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(314, 35);
+            this.label3.Location = new System.Drawing.Point(205, 35);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(113, 13);
             this.label3.TabIndex = 5;
@@ -92,7 +97,7 @@
             // lblbmsell
             // 
             this.lblbmsell.AutoSize = true;
-            this.lblbmsell.Location = new System.Drawing.Point(454, 35);
+            this.lblbmsell.Location = new System.Drawing.Point(345, 35);
             this.lblbmsell.Name = "lblbmsell";
             this.lblbmsell.Size = new System.Drawing.Size(0, 13);
             this.lblbmsell.TabIndex = 6;
@@ -100,7 +105,7 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(313, 66);
+            this.label4.Location = new System.Drawing.Point(204, 66);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(114, 13);
             this.label4.TabIndex = 7;
@@ -109,7 +114,7 @@
             // lblbmbuy
             // 
             this.lblbmbuy.AutoSize = true;
-            this.lblbmbuy.Location = new System.Drawing.Point(454, 66);
+            this.lblbmbuy.Location = new System.Drawing.Point(345, 66);
             this.lblbmbuy.Name = "lblbmbuy";
             this.lblbmbuy.Size = new System.Drawing.Size(0, 13);
             this.lblbmbuy.TabIndex = 8;
@@ -134,10 +139,10 @@
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(317, 116);
+            this.textBox2.Location = new System.Drawing.Point(207, 115);
             this.textBox2.Name = "textBox2";
             this.textBox2.ReadOnly = true;
-            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.Size = new System.Drawing.Size(111, 20);
             this.textBox2.TabIndex = 11;
             this.textBox2.Text = "https://bitmarket.eu/";
             // 
@@ -146,9 +151,9 @@
             this.textBox3.Location = new System.Drawing.Point(13, 115);
             this.textBox3.Name = "textBox3";
             this.textBox3.ReadOnly = true;
-            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.Size = new System.Drawing.Size(135, 20);
             this.textBox3.TabIndex = 12;
-            this.textBox3.Text = "http://www.tradehill.com";
+            this.textBox3.Text = "https://www.mtgox.com";
             // 
             // menuStrip1
             // 
@@ -167,11 +172,61 @@
             this.aboutToolStripMenuItem.Text = "&About";
             this.aboutToolStripMenuItem.Click += new System.EventHandler(this.aboutToolStripMenuItem_Click);
             // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(398, 35);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(109, 13);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "Tradehill Sell Price : $";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(397, 66);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(110, 13);
+            this.label7.TabIndex = 15;
+            this.label7.Text = "Tradehill Buy Price : $";
+            // 
+            // lblthsell
+            // 
+            this.lblthsell.AutoSize = true;
+            this.lblthsell.Location = new System.Drawing.Point(526, 35);
+            this.lblthsell.Name = "lblthsell";
+            this.lblthsell.Size = new System.Drawing.Size(35, 13);
+            this.lblthsell.TabIndex = 16;
+            this.lblthsell.Text = "label8";
+            // 
+            // lblthbuy
+            // 
+            this.lblthbuy.AutoSize = true;
+            this.lblthbuy.Location = new System.Drawing.Point(526, 66);
+            this.lblthbuy.Name = "lblthbuy";
+            this.lblthbuy.Size = new System.Drawing.Size(35, 13);
+            this.lblthbuy.TabIndex = 17;
+            this.lblthbuy.Text = "label8";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(401, 115);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.ReadOnly = true;
+            this.textBox4.Size = new System.Drawing.Size(160, 20);
+            this.textBox4.TabIndex = 18;
+            this.textBox4.Text = "https://tradehill.com";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(625, 262);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.lblthbuy);
+            this.Controls.Add(this.lblthsell);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
             this.Controls.Add(this.textBox3);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
@@ -180,15 +235,15 @@
             this.Controls.Add(this.label4);
             this.Controls.Add(this.lblbmsell);
             this.Controls.Add(this.label3);
-            this.Controls.Add(this.lblThBuy);
+            this.Controls.Add(this.lblMtGoxBuy);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.lblTradeHillSell);
+            this.Controls.Add(this.lblMtGoxSell);
             this.Controls.Add(this.menuStrip1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
-            this.Text = "Bitcoin Price Comparison v 1.0000 by dubbz82";
+            this.Text = "Bitcoin Price Comparison v 1.2000 by dubbz82";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
@@ -199,10 +254,10 @@
 
         #endregion
 
-        private System.Windows.Forms.Label lblTradeHillSell;
+        private System.Windows.Forms.Label lblMtGoxSell;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label lblThBuy;
+        private System.Windows.Forms.Label lblMtGoxBuy;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label lblbmsell;
         private System.Windows.Forms.Label label4;
@@ -213,6 +268,11 @@
         private System.Windows.Forms.TextBox textBox3;
         private System.Windows.Forms.MenuStrip menuStrip1;
         private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label lblthsell;
+        private System.Windows.Forms.Label lblthbuy;
+        private System.Windows.Forms.TextBox textBox4;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -8,73 +8,149 @@ using System.Windows.Forms;
 using System.Net;
 
 
-
 namespace WindowsFormsApplication1
 {
     public partial class Form1 : Form
     {
 
+        //some of these will be later casted on the fly to actual numbers for comparison....that will probably be implemented later on....
         string content;
         string content2;
-        string thsell;
+        string mtgoxsell;
+        string mtgoxbuy;
+        string bmbuy;
+        string bmsell;
         string thbuy;
-        string bmbuy;//for use when mtgox decides to fix itself up ;)
-        string bmsell;//same as above.
+        string thsell;
         
-       // 
+      
         WebClient client = new WebClient();
         
         
         public Form1()
         {
-            ServicePointManager.ServerCertificateValidationCallback = (sender, certificate, chain, sslPolicyErrors) => { return true; };
+          
             InitializeComponent();
           
         }
         private void Form1_Load(object sender, EventArgs e)
         {
-            try
-            {
-                content = client.DownloadString(@"https://api.tradehill.com/APIv1/USD/Ticker");
-
-                string[] split = content.Split(new char[] { '\"' });
-                thsell = (split[5]);
-                thbuy = (split[9]);
-            }
-            catch //in case api is inaccessable..
-            {
-                MessageBox.Show("Tradehill data could not be pulled.....setting values to zero instead....");
-                thbuy = "0";
-                thsell = "0";
-            }
-         lblTradeHillSell.Text = thsell;
-         lblThBuy.Text = thbuy.ToString();
-     // double thsellnum =double.Parse(thsell);for later use....
-    //double thbuynum = double.Parse(thbuy); also for later use...
-         try
-         {
-
-             content2 = client.DownloadString(@"https://bitmarket.eu/api/ticker");
-             string[] split2 = content2.Split(new char[] { '\"' });
-             bmbuy = (split2[57]);
-             bmsell = (split2[53]);
-         }
-         catch
-         {
-             MessageBox.Show("bitmarket data could not be pulled.....setting values to zero instead....");
-             bmbuy = "0";
-             bmsell = "0";
-         }
-          lblbmsell.Text = bmsell;
-          lblbmbuy.Text = bmbuy;
-
-          //  MessageBox.Show(content2.ToString());
-            
+            PullData();
         }
 
         private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("This is a simple program used to pull the current price values from tradehill and bitmarket....support for MtGox (hopefully) coming soon.  Right now, there is no real validation so if the program cannot connect to one of the two sites, it may very well crash or cause unexpected results...this should be fixed in the future.  Feel free to donate to the supplied address, and I will continue to try and improve upon this in my spare time....");
+            MessageBox.Show("This is a simple program used to pull the current price values from MtGox (finally), tradehill, and bitmarket.  Autorefreshing coming soon.  Feel free to donate to the supplied address, and I will continue to try and improve upon this in my spare time....");
+
+        }
+
+
+
+
+
+        public void PullData()
+        {
+            try
+            {
+                //this took a good amount of googling to figure out how to get it to work correctly....thank you mtgox for keeping to standard API's that just work like the rest of the world :/
+                // used to build entire input
+                StringBuilder sb = new StringBuilder();
+
+                // used on each read operation
+                byte[] buf = new byte[8192];
+
+                // prepare the web page we will be asking for
+                HttpWebRequest request = (HttpWebRequest)
+                    WebRequest.Create("https://mtgox.com/code/data/ticker.php");
+                request.ContentType = "application/x-www-form-urlencoded";
+                request.Method = "POST";
+                request.UserAgent = "Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; WOW64; Trident/5.0)";
+
+                // execute the request
+                HttpWebResponse response = (HttpWebResponse)
+                    request.GetResponse();
+
+                // read data via the response stream
+                System.IO.Stream resStream = response.GetResponseStream();
+
+                string tempString = null;
+                int count = 0;
+
+                do
+                {
+                    // fill the buffer with data
+                    count = resStream.Read(buf, 0, buf.Length);
+
+                    // make sure we read some data
+                    if (count != 0)
+                    {
+                        // translate from bytes to ASCII text
+                        tempString = Encoding.ASCII.GetString(buf, 0, count);
+
+                        // continue building the string
+                        sb.Append(tempString);
+                    }
+                }
+                while (count > 0); // any more data to read?
+
+                
+
+                string[] split = sb.ToString().Split(new char[] { ':' });  //pull buy and sell data from imported data
+                mtgoxbuy = split[7];
+                mtgoxsell = split[8];
+                split = mtgoxbuy.Split(new char[] { ',' });
+                mtgoxbuy = split[0];
+                split = mtgoxsell.Split(new char[] { '}' });
+                mtgoxsell = split[0];
+              
+               
+            }
+            catch //in case api is inaccessable..
+            {
+                MessageBox.Show("Mtgox data could not be pulled.....setting values to zero instead....");
+                mtgoxbuy = "0";
+                mtgoxsell = "0";
+            }
+            lblMtGoxSell.Text = mtgoxsell;
+            lblMtGoxBuy.Text = mtgoxbuy;
+
+            try
+            {
+
+
+                content2 = client.DownloadString(@"https://bitmarket.eu/api/ticker");
+
+
+                string[] split2 = content2.Split(new char[] { '\"' });
+                bmbuy = (split2[57]);
+                bmsell = (split2[53]);
+            }
+            catch //in case api is inaccessable
+            {
+                MessageBox.Show("bitmarket data could not be pulled.....setting values to zero instead....");
+                bmbuy = "0";
+                bmsell = "0";
+            }
+            lblbmsell.Text = bmsell;
+            lblbmbuy.Text = bmbuy;
+
+            try
+            {
+                content = client.DownloadString(@"https://api.tradehill.com/APIv1/USD/Ticker");
+                string[] split3 = content.Split(new char[] { '"' });
+                thbuy = split3[9];
+                thsell = split3[5];
+            }
+
+            catch //in case api is inaccessable
+            { 
+                MessageBox.Show("tradehill data could not be pulled......setting values to zero instead....");
+                thbuy = "0";  
+                thsell = "0"; 
+               
+            }
+            lblthbuy.Text = thbuy;
+            lblthsell.Text = thsell;
 
         }
     }
